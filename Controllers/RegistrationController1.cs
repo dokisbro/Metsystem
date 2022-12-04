6c70b4d3d0da666e9797fa2357b09fa820c59ba3
Кирилл Поляков <polakov934@gmail.com>
2022-12-04 14:16:38 +0500

    Фон+картинка

---
+++ Controllers/RegistrationController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Журна_ты.Controllers
+{
+    public class RegistrationController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
